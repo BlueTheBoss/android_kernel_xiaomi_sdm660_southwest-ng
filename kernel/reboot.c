65949d62be496eccdca413728637a14b1a5aa4f6
armevox <armevox@users.noreply.github.com>
2026-06-03 10:24:42 +0000

    tulip: Fix KernelSU-Next reboot hook parameter mismatch

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -306,7 +306,7 @@ DEFINE_MUTEX(system_transition_mutex);
  * reboot doesn't sync: do that yourself before calling this.
  */
 #ifdef CONFIG_KSU
-extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user *arg);
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
 
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
@@ -317,7 +317,7 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	int ret = 0;
 
 #ifdef CONFIG_KSU
-	ksu_handle_sys_reboot(magic1, magic2, cmd, arg);
+	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
 #endif
 
 	/* We only trust the superuser with rebooting the system. */
